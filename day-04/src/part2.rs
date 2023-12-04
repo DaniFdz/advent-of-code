cff55bf8e00716027f2f4fe10c5bc31ec485af03
DaniFdz <danifernandezzzzzz@gmail.com>
2023-12-04 13:48:02 +0100

    Day 4 part 2

--- day-04/src/part2.rs
+++ day-04/src/part2.rs
@@ -1,5 +1,58 @@
-pub fn process(input: &str) -> u16 {
-    let mut sum: u16 = 0;
+use std::collections::HashMap;
+
+fn get_winning_numbers(mut winning_card: Vec<u16>, actual_card: Vec<u16>) -> u16{
+    let mut count = 0;
+    winning_card.sort();
+    for number in actual_card {
+        if binary_search(&winning_card, number) {
+            count += 1;
+        }
+    }
+    count
+}
+
+fn binary_search(winning_card: &[u16], number: u16) -> bool {
+   let mut left = 0;
+   let mut right = winning_card.len();
+   while left < right {
+       let mid = (left + right) / 2;
+       if winning_card[mid] == number {
+           return true;
+       } else if winning_card[mid] < number {
+           left = mid + 1;
+       } else {
+           right = mid;
+       }
+   }
+   false
+}
+
+pub fn process(input: &str) -> u32 {
+    let mut sum: u32 = 0;
+    let mut copies: HashMap<u16, u32> = HashMap::new();
+    input.lines().enumerate().for_each(|(i,_)| {
+        copies.insert(i as u16, 1);
+    });
+    
+    for (i,line) in input.lines().enumerate() {
+        let number_of_copies = *copies.get(&(i as u16)).unwrap();
+        if number_of_copies == 0 {
+            break;
+        }
+        let cards = line.split(": ").collect::<Vec<&str>>()[1].split(" | ").collect::<Vec<&str>>();
+        let winning_card = cards[0].split(" ").filter_map(|x| x.parse::<u16>().ok()).collect::<Vec<u16>>();
+        let actual_card = cards[1].split(" ").filter_map(|x| x.parse::<u16>().ok()).collect::<Vec<u16>>();
+        let winning_numbers = get_winning_numbers(winning_card, actual_card);
+        for j in i+1..i+1+winning_numbers as usize {
+            if j == input.lines().count() {
+                break;
+            }
+            *copies.get_mut(&(j as u16)).unwrap() += number_of_copies;
+        }
+    }
+    for i in 0..input.lines().count() {
+        sum += *copies.get(&(i as u16)).unwrap() as u32;
+    }
     sum
 }
 
@@ -9,8 +62,13 @@ mod tests{
 
     #[test]
     fn test_part2(){
-        let result = process("");
-        assert_eq!(result, 0);
+        let result = process("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11");
+        assert_eq!(result, 30);
     }
 }
 
