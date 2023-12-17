0cb72e6ee02209d6fa6be07e7fdca02c8e9d15a5
DaniFdz <danifernandezzzzzz@gmail.com>
2023-12-17 12:45:08 +0100

    Day 13 p1

---
+++ day-14/src/part1.rs
@@ -0,0 +1,77 @@
+use std::collections::HashMap;
+
+fn transpose(input: &str) -> Vec<Vec<char>> {
+    let mut result: Vec<Vec<char>> = Vec::new();
+    for x in 0..input.lines().nth(0).unwrap().len() {
+        let mut row: Vec<char> = Vec::new();
+        for y in 0..input.lines().count() {
+            row.push(input.lines().nth(y).unwrap().chars().nth(x).unwrap());
+        }
+        result.push(row);
+    }
+    result
+}
+
+fn treat_line(input: &Vec<char>, cache: &mut HashMap<Vec<char>, u64>) -> u64{
+    if let Some(x) = cache.get(input) {
+        return *x;
+    }
+    if input.len() == 0 {
+        return 0;
+    }
+
+    let mut count = 0;
+    let mut i = 0;
+    while i < input.len() && input[i] != '#' {
+        if input[i] == 'O' {
+            count += 1;
+        }
+        i += 1;
+    }
+    let mut result = 0;
+    if i < input.len()-1 {
+        let slice = &input[i+1..].to_vec();
+        result += treat_line(slice, cache);
+    }
+    count = input.len() as u64 - count;
+    result += (input.len()*(input.len()+1)/2) as u64 - (count*(count+1)/2) as u64;
+    cache.insert(input.to_vec(), result);
+    result
+}
+
+pub fn process(input: &str) -> u64 {
+    let data = transpose(input);
+    let mut cache: HashMap<Vec<char>, u64> = HashMap::new();
+    data.iter().map(|x| treat_line(x, &mut cache)).sum()
+}
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn test_transpose(){
+        let result = transpose("O.");
+        assert_eq!(result, vec![vec!['O'], vec!['.']]);
+        let result = transpose("O.
+O.");
+        assert_eq!(result, vec![vec!['O','O'],vec!['.','.']]);
+
+    }
+
+    #[test]
+    fn test_part1(){
+        let result = process("O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....");
+        assert_eq!(result, 136);
+    }
+}
+
